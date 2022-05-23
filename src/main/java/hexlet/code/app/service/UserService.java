df4606e7db1716ac93b6888f43c3bc7181ca5bcd
Alex L <12oprs@gmail.com>
2022-05-23 13:13:08 +1200

    add pass hashing

--- src/main/java/hexlet/code/app/service/UserService.java
+++ src/main/java/hexlet/code/app/service/UserService.java
@@ -2,47 +2,50 @@ package hexlet.code.app.service;
 
 import hexlet.code.app.Mapper;
 import hexlet.code.app.dto.UserCreationDTO;
-import hexlet.code.app.dto.UserDTO;
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public final class UserService {
     @Autowired
     private UserRepository repository;
     @Autowired
     private Mapper mapper;
+    @Autowired
+    private PasswordEncoder encoder;
 
     public List<User> getUsers() {
         return (List<User>) repository.findAll();
     }
 
     public User getUser(long id) throws Exception {
-        return repository.findById(id).orElseThrow(() -> new Exception("User not exist"));
+        return repository.findById(id).orElseThrow(() -> new Exception("User not found"));
     }
 
     public User createUser(UserCreationDTO dto) {
+        dto.setPassword(encoder.encode(dto.getPassword()));
         User user = mapper.toUser(dto);
         return repository.save(user);
     }
     public User updateUser(long id, UserCreationDTO dto) throws Exception {
-        User updatedUser = repository.findById(id).orElseThrow(() -> new Exception("User not exist"));
+        User updatedUser = repository.findById(id).orElseThrow(() -> new Exception("Can't update. User not found"));
         updatedUser.setFirstName(dto.getFirstName());
         updatedUser.setLastName(dto.getLastName());
         updatedUser.setEmail(dto.getEmail());
-        updatedUser.setPassword(dto.getPassword());
+        updatedUser.setPassword(encoder.encode(dto.getPassword()));
         return repository.save(updatedUser);
     }
 
-    public String deleteUser(long id) {
+    public String deleteUser(long id) throws Exception {
         if (repository.existsById(id)) {
             repository.deleteById(id);
             return "User deleted";
         }
-        return "User not exist";
+        throw new Exception("Can't delete. User not exist");
     }
 }
