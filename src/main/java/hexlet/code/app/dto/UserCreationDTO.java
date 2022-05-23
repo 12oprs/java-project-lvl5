0a041e24f2fd593303b23c1160b6f6ea9ed7d06e
Alex L <12oprs@gmail.com>
2022-05-23 13:14:53 +1200

    add constructors to DTOs

--- src/main/java/hexlet/code/app/dto/UserCreationDTO.java
+++ src/main/java/hexlet/code/app/dto/UserCreationDTO.java
@@ -1,10 +1,12 @@
 package hexlet.code.app.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserCreationDTO extends UserDTO {
 
     private String password;
--- src/main/java/hexlet/code/app/dto/UserDTO.java
+++ src/main/java/hexlet/code/app/dto/UserDTO.java
@@ -1,10 +1,12 @@
 package hexlet.code.app.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDTO {
     private String firstName;
     private String lastName;
