60ac1b6489ed3ce64df8976779845fb070dc91ff
Alex L <12oprs@gmail.com>
2022-07-18 14:45:02 +1200

    add TestUtils (for authorizedRequests in tests)

---
+++ src/test/java/hexlet/code/app/TestUtils.java
@@ -0,0 +1,41 @@
+package hexlet.code.app;
+
+import hexlet.code.app.component.JWTHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY;
+
+@Component
+public final class TestUtils {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    JWTHelper jwtHelper;
+
+    public String getToken(String username) {
+        final String token = jwtHelper.expiring(Map.of(SPRING_SECURITY_FORM_USERNAME_KEY, username));
+        return token;
+    }
+
+    public ResultActions authorizedRequest(final MockHttpServletRequestBuilder request, final String username)
+            throws Exception {
+        final String token = jwtHelper.expiring(Map.of("username", username));
+        request.header(AUTHORIZATION, token);
+
+        return perform(request);
+    }
+
+    public ResultActions perform(final MockHttpServletRequestBuilder request) throws Exception {
+        return mockMvc.perform(request);
+    }
+
+}
