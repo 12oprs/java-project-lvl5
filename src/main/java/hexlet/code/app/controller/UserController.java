a55d5845b80043afaf2096b9c5da588a56ca8f3a
Alex L <12oprs@gmail.com>
2022-07-21 16:54:21 +1200

    fix user controller

--- src/main/java/hexlet/code/app/controller/UserController.java
+++ src/main/java/hexlet/code/app/controller/UserController.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,7 +24,7 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "${base-url}" + "/users")
-public class UserController<ONLY_OWNER_BY_ID> {
+public class UserController {
 
     @Autowired
     private UserService service;
@@ -60,7 +60,7 @@ public class UserController<ONLY_OWNER_BY_ID> {
         return service.createUser(dto);
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     @PreAuthorize(ONLY_OWNER_BY_ID)
     public User updateUser(@PathVariable long id, @RequestBody UserCreationDTO dto) {
         User user = null;
