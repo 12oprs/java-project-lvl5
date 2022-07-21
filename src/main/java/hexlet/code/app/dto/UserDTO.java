cb3b2a91389d441048e9e8a978020e6f39b1b3a1
Alex L <12oprs@gmail.com>
2022-07-21 16:56:14 +1200

    fix UserDTO

--- src/main/java/hexlet/code/app/dto/UserDTO.java
+++ src/main/java/hexlet/code/app/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,14 +8,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+
     private String firstName;
     private String lastName;
     private String email;
 
-    public UserDTO(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
 }
